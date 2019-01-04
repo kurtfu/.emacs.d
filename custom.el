972b3e2a3d786972541be56e03cc54a39ff70779
Furkan Kurt <kurtfu@yahoo.com>
2019-01-04 10:53:23 +0300

    Add custom file

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(dumb-jump-prefer-searcher (quote ag))
+ '(package-selected-packages
+   (quote
+    (git-gutter spacemacs-theme smartparens neotree magit flycheck dumb-jump auto-complete all-the-icons)))
+ '(require-final-newline t)
+ '(scroll-conservatively 1000)
+ '(scroll-step 1))
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
